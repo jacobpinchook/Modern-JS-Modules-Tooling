0565fb6b02124bb5fbbb60554a53e268c5660e05
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-21 16:21:17 -0500

    Exporting and importing in ES6 Modules

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+// Importing module
+// import { 
+//     addToCart, 
+//     totalPrice as price, 
+//     tq 
+// } from './shoppingCart.js';
+
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+
+import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.totalPrice);
