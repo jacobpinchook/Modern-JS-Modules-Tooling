ebd8aa97e13ac8f4bb61cde562cd808318266634
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-21 16:47:55 -0500

    Applied top-level await demo

--- starter/script.js
+++ starter/script.js
@@ -25,8 +25,25 @@ add('apples', 4);
 // Imports/Exports are a live connection, not a static copy
 console.log(cart);
 
-console.log('Start fetch');
-const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-const data = await res.json();
-console.log(data);
-console.log('Something');
+// Simple top-level await
+// console.log('Start fetch');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+
+    return {title: data.at(-1).title, text: data.at(-1).body};
+}
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
