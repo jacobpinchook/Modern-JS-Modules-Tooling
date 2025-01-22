cbf07efb8c983baeb4f9526aea5e93dd235a9036
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-22 16:49:52 -0500

    CommonJS module practice

--- starter/script.js
+++ starter/script.js
@@ -52,6 +52,10 @@ console.log(lastPost2);
 
 */
 
+// -------------------------------------
+
+/*
+
 const ShoppingCart2 = (function() {
     const cart = [];
     const shippingCost = 10;
@@ -83,3 +87,21 @@ ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+
+*/
+
+// -------------------------------------
+
+/*
+
+// CommonJS Modules
+// Export
+export.addToCart = function(product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} ordered from supplier`);
+};
+
+// Import - Used in NodeJS
+const { addToCart } = require('./shoppingCart.js');
+
+*/
