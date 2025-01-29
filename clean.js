1afd57e9112b28b217fe7e1c769bae70472fbd40
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-01-29 11:40:27 -0500

    Clean more code

--- clean.js
+++ clean.js
@@ -20,16 +20,14 @@ const addExpense = function (value, description, user = 'jonas') {
   user = user.toLowerCase();
 
   // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
-  const limit = getLimit(user);
 
-  if (value <= limit) {
+  if (value <= getLimit(user)) {
     budget.push({ value: -value, description, user });
   }
 };
 addExpense(10, 'Pizza 🍕');
 addExpense(100, 'Going to movies 🍿', 'Matilda');
 addExpense(200, 'Stuff', 'Jay');
-console.log(budget);
 
 const checkExpenses = function () {
   for (const entry of budget)
@@ -38,16 +36,16 @@ const checkExpenses = function () {
 };
 checkExpenses();
 
-console.log(budget);
-
-const bigExpenses = function (limit) {
+const logBigExpenses = function (bigLimit) {
   let output = '';
-  for (const el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+  for (const entry of budget) {
+    if (entry.value <= -bigLimit) {
+      output += `${entry.description.slice(-2)} / `; // Emojis are 2 chars
     }
   }
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
-bigExpenses(1000);
+
+console.log(budget);
+logBigExpenses(500);
